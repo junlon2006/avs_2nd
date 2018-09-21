ca336deafbe7bc1b148f219e5ec13702b7cf649d
shangjinlong <shangjinlong@unisound.com>
2018-09-21 11:31:18 +0800

    Update 3rd

---
+++ sdk-folder/third-party/usr/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/junlon2006/workspace/amazon/sdk-folder/sdk-build/ARM/libxml2/lib"
+XML2_LIBS="-lxml2 /home/junlon2006/workspace/amazon/sdk-folder/sdk-build/ARM/zlib/lib/libz.a     -lm "
+XML2_INCLUDEDIR="-I/home/junlon2006/workspace/amazon/sdk-folder/sdk-build/ARM/libxml2/include/libxml2"
+MODULE_VERSION="xml2-2.9.8"
+
